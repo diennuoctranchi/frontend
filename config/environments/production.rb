c677788d3180320bb8b2fbbef2fa9cceb7d2398b
Dien Nuoc Tran Chi <diennuoctranchi@gmail.com>
2021-11-08 02:10:05 +0700

    size limit for log file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.logger = ActiveSupport::Logger.new(config.paths['log'].first, 1, 50 * 1024 * 1024)
 end
